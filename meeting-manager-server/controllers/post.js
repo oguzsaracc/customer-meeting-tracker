f4892c66d98e7417d2b9dd5440eb3384538e656b
Oguz Sarac <oguzsarac0@gmail.com>
2023-10-27 22:58:03 +0100

    Implement post creation logic in controller
    In the 'post.js' controller, added logic to create a post using data received from the request body. Utilized the 'slugify' library to generate a user-friendly URL slug from the post title. Implemented basic validation to ensure that 'title' and 'content' fields are provided before creating the post. The post is then created in the database using the 'Post.create' method.

--- meeting-manager-server/controllers/post.js
+++ meeting-manager-server/controllers/post.js
@@ -1,5 +1,20 @@
 const Post = require("../models/post");
+const slugify = require("slugify");
 
 exports.create = (req, res) => {
-  //console.log(req.body);
+  // console.log(req.body);
+  const { title, content, user } = req.body;
+  const slug = slugify(title); // The title of the customers will be passed in our api. For example: Michael Son -> api/michael-son
+
+  // Validation - Before the creation of post method we are validating the data.
+  switch (true) {
+    case !title:
+      return res.status(400).json({ error: "Title is required" });
+      break;
+    case !content:
+      return res.status(400).json({ error: "Content is required" });
+      break;
+  }
+
+  Post.create({ title, content, user, slug });
 };
