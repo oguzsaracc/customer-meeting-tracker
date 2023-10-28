49877d728c0bc2dd6fc9699e1bd85263c2b6fa11
Oguz Sarac <oguzsarac0@gmail.com>
2023-10-28 16:58:15 +0100

    Updated Create Component with State Management and Event Handling
    Implemented state management using useState hook to handle user input in the Create.js component. Added event handlers for updating state based on user interactions with form fields.

--- meeting-manager/src/Create.js
+++ meeting-manager/src/Create.js
@@ -1,45 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Create = () => (
-  <div className="container p-5">
-    <h1>Create Customer Meeting Notes</h1>
-    <br />
-    <form>
-      <div className="form-group">
-        <label className="text-muted">Customer Title</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Please enter customer's title"
-          required
-        />
-      </div>
+const Create = () => {
+  const [state, setState] = useState({
+    // The empty states that we created for the data that we will receive from the user.
+    title: "",
+    content: "",
+    user: "",
+  });
+
+  // Destructring the values from the states for simpler implementation in value.
+  const { title, content, user } = state;
+
+  // Creating a onChange to create an event handler for input changes.
+  const handleChange = (name) => (event) => {
+    // console.log("name", name, "event", event.target.value);
+    setState({ ...state, [name]: event.target.value });
+  };
+
+  return (
+    <div className="container p-5">
+      <h1>Create Customer Meeting Notes</h1>
       <br />
-      <div className="form-group">
-        <label className="text-muted">Customer details</label>
-        <textarea
-          type="text"
-          className="form-control"
-          placeholder="Please enter customer's details"
-          required
-        />
-      </div>
-      <br />
-      <div className="form-group">
-        <label className="text-muted">User</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Please enter your name"
-          required
-        />
-      </div>
-      <br />
-      <div>
-        <button className="btn btn-primary">Create Note</button>
-      </div>
-    </form>
-  </div>
-);
+      <form>
+        <div className="form-group">
+          <label className="text-muted">Customer Title</label>
+          <input
+            onChange={handleChange("title")}
+            value={title}
+            type="text"
+            className="form-control"
+            placeholder="Please enter customer's title"
+            required
+          />
+        </div>
+        <br />
+        <div className="form-group">
+          <label className="text-muted">Customer details</label>
+          <textarea
+            onChange={handleChange("content")}
+            value={content}
+            type="text"
+            className="form-control"
+            placeholder="Please enter customer's details"
+            required
+          />
+        </div>
+        <br />
+        <div className="form-group">
+          <label className="text-muted">Employee's name</label>
+          <input
+            onChange={handleChange("user")}
+            value={user}
+            type="text"
+            className="form-control"
+            placeholder="Please enter your name"
+            required
+          />
+        </div>
+        <br />
+        <div>
+          <button className="btn btn-primary">Create Note</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Create;
